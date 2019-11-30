812aeb735575a86532980d75195f4f9336b87ddf
Álvaro Villalba <alvaro.vica@hotmail.com>
2019-12-01 00:34:10 +0100

    create some folders to order the code

--- aframe-island-component/dist/aframe-island-component.js
+++ aframe-island-component/dist/aframe-island-component.js
@@ -109,3 +109,118 @@ eval("/* global AFRAME */\nif (typeof AFRAME === 'undefined') {\n  throw new Err
 
 /******/ });
 });
+
+/* global AFRAME */
+
+if (typeof AFRAME === 'undefined') {
+	throw new Error('Component attempted to register before AFRAME was available.');
+  }
+  
+  /**
+   * Island component for A-Frame.
+   */
+AFRAME.registerComponent('island', {
+	schema: {
+	  id: {type: 'number', default: 0},
+		depth: {type: 'number', default: 1},
+		height: {type: 'number', default: 1},
+		width: {type: 'number', default: 1},
+	},
+  
+	/**
+	 * Set if component needs multiple instancing.
+	 */
+	multiple: false,
+  
+	/**
+	 * Called once when component is attached. Generally for initial setup.
+	 */
+	init: function () { },
+  
+	/**
+	 * Called when component is attached and when component data changes.
+	 * Generally modifies the entity based on the data.
+	 */
+	update: function (oldData) { },
+  
+	/**
+	 * Called when a component is removed (e.g., via removeAttribute).
+	 * Generally undoes all modifications to the entity.
+	 */
+	remove: function () { },
+  
+	/**
+	 * Called on each scene tick.
+	 */
+	// tick: function (t) { },
+  
+	/**
+	 * Called when entity pauses.
+	 * Use to stop or remove any dynamic or background behavior such as events.
+	 */
+	pause: function () { },
+  
+	/**
+	 * Called when entity resumes.
+	 * Use to continue or add any dynamic or background behavior such as events.
+	 */
+	play: function () { },
+  
+	/**
+	 * Event handlers that automatically get attached or detached based on scene state.
+	 */
+	events: {
+	  // click: function (evt) { }
+	}
+  });
+  
+  AFRAME.registerComponent('islands', {
+	schema: {},
+  
+	/**
+	 * Set if component needs multiple instancing.
+	 */
+	multiple: false,
+  
+	/**
+	 * Called once when component is attached. Generally for initial setup.
+	 */
+	init: function () { },
+  
+	/**
+	 * Called when component is attached and when component data changes.
+	 * Generally modifies the entity based on the data.
+	 */
+	update: function (oldData) { },
+  
+	/**
+	 * Called when a component is removed (e.g., via removeAttribute).
+	 * Generally undoes all modifications to the entity.
+	 */
+	remove: function () { },
+  
+	/**
+	 * Called on each scene tick.
+	 */
+	// tick: function (t) { },
+  
+	/**
+	 * Called when entity pauses.
+	 * Use to stop or remove any dynamic or background behavior such as events.
+	 */
+	pause: function () { },
+  
+	/**
+	 * Called when entity resumes.
+	 * Use to continue or add any dynamic or background behavior such as events.
+	 */
+	play: function () { },
+  
+	/**
+	 * Event handlers that automatically get attached or detached based on scene state.
+	 */
+	events: {
+	  // click: function (evt) { }
+	}
+  });
+  
